91a59b3cf6498a01a2c334fc040643675d0a6beb
Vladislav Silantev <silyaka@yandex.ru>
2023-10-24 17:43:49 +0300

    unused code removed

--- Component/Garden/Facade.php
+++ Component/Garden/Facade.php
@@ -4,22 +4,10 @@ namespace Component\Garden;
 
 class Facade {
 
-    private FruitProcessor  $_fruitProcessor;
-    private TreeProcessor   $_treeProcessor;
     private GardenProcessor $_gardenProcessor;
 
     public function __construct() {
-        $this->_fruitProcessor  = new FruitProcessor();
-        $this->_treeProcessor   = new TreeProcessor();
         $this->_gardenProcessor = new GardenProcessor();
-    }
-
-    public function createApple(int $treeId): Entity\Fruit\Apple {
-        return $this->_fruitProcessor->createApple($treeId);
-    }
-
-    public function createPearl(int $treeId): Entity\Fruit\Pearl {
-        return $this->_fruitProcessor->createPearl($treeId);
     }
 
     public function createDefaultGarden(): Entity\Garden {
